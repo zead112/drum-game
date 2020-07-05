8823eab160550f6524c1f3ab6d05a39692198373
zead <zeadhajebrahem123@gmail.com>
2020-07-05 13:59:53 +0300

    init

---
+++ index.js
@@ -0,0 +1,66 @@
+//detecting button press
+var numberofdrumbutton= document.querySelectorAll(".drum").length;
+for(var i=0; i<numberofdrumbutton; i++ ){
+document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+var buttoninnerHTML =this.innerHTML;
+
+makesound(buttoninnerHTML);
+buttonAnimatuin(buttoninnerHTML);
+});;
+
+} 
+//detecting keyboard press
+document.addEventListener("keypress",function(event){
+   makesound(event.key);
+   buttonAnimatuin(event.key);
+
+   
+   
+});
+function makesound(key) {
+   switch(key) {
+      case "w":
+         var tom1= new Audio("sounds/tom-1.mp3");
+         tom1.play();
+      break;
+       
+      case "a":
+         var tom2= new Audio("sounds/tom-2.mp3");
+         tom2.play();
+        break;
+        case "s":
+          var tom3= new Audio("sounds/tom-3.mp3");
+         tom3.play();
+         break;
+         case "d":
+         var tom4= new Audio("sounds/tom-4.mp3");
+         tom4.play();
+        break;
+        case "j":
+         var tom2= new Audio("sounds/snare.mp3");
+         tom2.play();
+         break;
+   case "k":
+   var crash= new Audio("sounds/crash.mp3");
+      crash.play();
+   break;  
+   case "l":
+   var kick= new Audio("sounds/kick-bass.mp3");
+   kick.play();
+   break;             
+   
+   
+   
+      default:console.log(buttoninnerHTML);
+         
+   }
+   
+   
+}
+function buttonAnimatuin(currentkey){
+   var activebutton = document.querySelector("." + currentkey);
+   activebutton.classList.add("pressed") 
+   setTimeout(function(){
+      activebutton.classList.remove("pressed");
+   },100);
+}
